d8b73247e5f4464f2ae495dcbb6148bd0b8c4826
DEX3D <61697687+burasate@users.noreply.github.com>
2021-05-30 21:34:04 +0700

    Update mornitor.py

--- mornitor.py
+++ mornitor.py
@@ -150,6 +150,7 @@ def MornitoringUser(idName):
     morn_df['Profit%'] = ((morn_df['Market'] - morn_df['Buy']) / morn_df['Buy']) * 100
     morn_df['Profit%'] = morn_df['Profit%'].round(2)
     morn_df.loc[morn_df['Profit%'] < 0.0, 'Max_Drawdown%'] = morn_df['Profit%'].abs()
+    morn_df.loc[morn_df['Profit%'] > 0.0, 'Max_Drawdown%'] = 0.0
     morn_df['Max_Drawdown%'] = morn_df.groupby(['User', 'Symbol'])['Max_Drawdown%'].transform('max')
     morn_df.drop_duplicates(['User','Symbol'],keep='last',inplace=True)
     morn_df.to_csv(mornitorFilePath, index=False)
