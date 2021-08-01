45fa13f111dc79d663bcba2eab9b53c80032d160
DEX3D <61697687+burasate@users.noreply.github.com>
2021-08-01 19:56:12 +0700

    Update realtime.py

--- realtime.py
+++ realtime.py
@@ -440,20 +440,21 @@ def Realtime(idName,sendNotify=True):
     balance = getBalance(idName)
     if balance != None: #Have Secret API
         portfolioList = port_df['Symbol'].tolist()
+        print(portfolioList)
         balanceList = []
         dropList = []
         for sym in balance: #Check Real Balance
             if balance[sym]['available'] != 0 and sym != 'THB': #if not THB and have available
                 symbol = 'THB_{}'.format(sym)
                 if not symbol in portfolioList: #Not balace in mornitor
-                    CreateSellOrder(idName, symbol, count=1)
+                    #CreateSellOrder(idName, symbol, count=1)
                     if sendNotify:
                         lineNotify.sendNotifyMassage(token, 'Clear {} in Balance'.format(symbol))
                     balanceList.append(symbol)
         for i in port_df.index.tolist(): #Check Mornitor
             row = port_df.loc[i]
             if not row['Symbol'] in balanceList:
-                dropList.append(row['Symbol'])
+                #dropList.append(row['Symbol'])
         for symbol in dropList: # Delete Fake Mornitor for User who have KeyAPI
             port_df = port_df[port_df['Symbol'] != symbol]
             if sendNotify:
