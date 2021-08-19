7c8413dcc21590e7ff211bd1a10d5428d378ea10
DEX3D <61697687+burasate@users.noreply.github.com>
2021-08-20 02:40:23 +0700

    Update realtime.py

--- realtime.py
+++ realtime.py
@@ -285,6 +285,7 @@ def Realtime(idName,sendNotify=True):
         ]
     signal_df = signal_df.sort_values(['Change4HR%','Volume','Risk%'], ascending=[True,False,True])
     signal_df.reset_index(inplace=True)
+    # Signal All Dataframe
     signal_df_all = signal_df
 
     # New Column For Signal DF
@@ -453,7 +454,7 @@ def Realtime(idName,sendNotify=True):
             #new_lossTarget = abs(row['Profit%'])
             #new_lossTarget = ( abs(port_df['Max_Drawdown%'].mean()) + abs(row['Profit%']) ) * 0.5
             new_lossTarget = signal_df_all['NDay_Drawdown%'].mean()
-            new_lossTarget = round(new_lossTarget,1)
+            new_lossTarget = round(new_lossTarget,1) + 1.0
             gSheet.setValue('Config', findKey='idName', findValue=idName, key='percentageLossTarget', value=new_lossTarget)
 
         if triggerSellPos == 'Lower':
