57318793a5aac2e376f4cff856858dc3ffade311
burasate <61697687+burasate@users.noreply.github.com>
2021-12-02 00:18:21 +0700

    Update realtime.py

--- realtime.py
+++ realtime.py
@@ -566,7 +566,7 @@ def Realtime(idName,sendNotify=True):
 
     print('---------------------\nAuto Preset\n---------------------')
     if autoPreset:
-        dayScore = 5
+        dayScore = 10
         tran_df = pd.read_csv(transacFilePath)
         tran_df = tran_df[
             tran_df['epoch'] >= now - ((1 * 60 * 60 * 24) * dayScore)
