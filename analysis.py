574e00031e229c4012da2ba30f404af7eb16c8ca
burasate <61697687+burasate@users.noreply.github.com>
2023-01-17 00:25:37 +0700

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -493,7 +493,7 @@ analysis.get_analysis(r\'{3}\', \'{4}\', saveImage=True, showImage=False)
 
     is_posix = os.name == 'posix' #raspi os
     if is_posix:
-        subprocess.call(['bash', '-c', 'python -c \"{}\"'.format(command)])
+        subprocess.call(['bash', '-c', 'python3 -c \"{}\"'.format(command)])
     else:
         subprocess.call(
             [r'D:\GDrive\Documents\2021\bitkubPy\venv\Scripts\python.exe','-c', command]
